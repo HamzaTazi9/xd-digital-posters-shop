3a6f8707d8f883697db6f420ede4aeca0378fc7d
r1050902 <r1050902@student.thomasmore.be>
2026-01-07 15:18:32 +0100

    feat(cart): winkelmand totaal en subtotaal toegevoegd - qty ondersteuning in session cart - totaalprijs berekening - knop om product te verwijderen

--- Project-root/cart.php
+++ Project-root/cart.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+
+if(!isset($_SESSION["cart"])){
+    $_SESSION["cart"] = [];
+}
 ?>
 
 <?php include_once("nav.inc.php"); ?>
@@ -9,7 +13,6 @@ session_start();
 <?php if(empty($_SESSION["cart"])): ?>
 
 <p>Je winkelmandje is leeg.</p>
-
 <a href="product.php">← Verder winkelen</a>
 
 <?php else: ?>
@@ -20,28 +23,41 @@ session_start();
     <th>Prijs</th>
     <th>Aantal</th>
     <th>Subtotaal</th>
+    <th>Actie</th>
 </tr>
 
 <?php 
 $total = 0;
 
 foreach($_SESSION["cart"] as $id => $item):
-    
-    $subtotal = $item["price"] * $item["quantity"];
+
+    $qty = $item["qty"] ?? $item["quantity"] ?? 1;
+
+    $subtotal = $item["price"] * $qty;
     $total += $subtotal;
 ?>
+
 <tr>
     <td><?php echo htmlspecialchars($item["name"]); ?></td>
-    <td>€ <?php echo $item["price"]; ?></td>
-    <td><?php echo $item["quantity"]; ?></td>
-    <td>€ <?php echo $subtotal; ?></td>
+
+    <td>€ <?php echo number_format($item["price"], 2); ?></td>
+
+    <td><?php echo $qty; ?></td>
+
+    <td>€ <?php echo number_format($subtotal, 2); ?></td>
+
+    <td>
+        <a href="cart_remove.php?id=<?php echo $id; ?>">
+            Verwijderen
+        </a>
+    </td>
 </tr>
 
 <?php endforeach; ?>
 
 </table>
 
-<h3>Totaal: € <?php echo $total; ?></h3>
+<h3>Totaal: € <?php echo number_format($total, 2); ?></h3>
 
 <a href="checkout.php">Bestelling plaatsen</a>
 
